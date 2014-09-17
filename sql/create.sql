2f0d9fe17d40181671668ac23aaba55408956528
TeeAaTeeU <teeaateeuu@gmail.com>
2014-09-17 17:19:26 +0300

    add primary keys to make querys faster

--- sql/create.sql
+++ sql/create.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS `ad_actions` (
   `date` date NOT NULL,
   `action` varchar(40) NOT NULL,
   `count` bigint(20) NOT NULL,
-  `value` bigint(20) NOT NULL
+  `value` bigint(20) NOT NULL,
+  PRIMARY KEY (`ad_id`,`date`,`action`)
 );
 
 CREATE TABLE IF NOT EXISTS `ad_statistics` (
@@ -11,5 +12,6 @@ CREATE TABLE IF NOT EXISTS `ad_statistics` (
   `date` date NOT NULL,
   `impressions` bigint(20) NOT NULL,
   `clicks` bigint(20) NOT NULL,
-  `spent` bigint(20) NOT NULL
+  `spent` bigint(20) NOT NULL,
+  PRIMARY KEY (`ad_id`,`date`)
 );
